383683b0dfd0ae55cf6f020f21ba697d6e93d5dc
Haider Bassem <haideralatbe@gmail.com>
2025-11-08 02:46:48 +0300

    Remove UI elements

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
-#include "Utils/ImageUtils.h"  
+#include "Utils/ImageUtils.h"
 
-int main() 
+int main()
 {
 
     auto img = ImageUtils::loadImage("/home/cpluspluser/Desktop/photo_2025-11-01_21-21-09.jpg", cv::IMREAD_COLOR);
     ImageUtils::saveImage(ImageUtils::convertToGray(img), "/home/cpluspluser/Desktop/test.png");
-    //std::cout<< ImageUtils::isValidImage("/home/cpluspluser/Desktop/Screenshot_20251103_204820.png",1);
+    if(ImageUtils::isValidImage("/home/cpluspluser/Desktop/photo_2025-11-01_21-21-09.jpg",1))
+        std::cout<<" \n\\n\ntest\n\n";
+    else
+        std::cout<< "\n\\n\nsad\n\n";
+
     return 0;
-    
+
 }
